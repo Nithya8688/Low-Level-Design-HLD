b2a4ed35a1233dd5bb160e47145fe3eb5434160b
Sourabh <nithyanandporika@gmail.com>
2026-03-01 22:24:41 +0530

    personal lap changes

--- src/main/java/com/example/ft/kafkaconfig/consumer/KafkaConsumerService.java
+++ src/main/java/com/example/ft/kafkaconfig/consumer/KafkaConsumerService.java
@@ -1,4 +1,15 @@
 package com.example.ft.kafkaconfig.consumer;
 
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class KafkaConsumerService {
+
+    @KafkaListener(topics = "demo-topic", groupId = "demo-group")
+    public void consume(String message) {
+        System.out.println("Consumed KAFKA message: " + message);
+    }
 }
--- src/main/java/com/example/ft/kafkaconfig/producer/KafkaProducerService.java
+++ src/main/java/com/example/ft/kafkaconfig/producer/KafkaProducerService.java
@@ -1,4 +1,18 @@
 package com.example.ft.kafkaconfig.producer;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("demo-topic", message);
+    }
 }
