0f0018694304a4b0da18f0976f9656b79717f045
Sabapatisourab Nithyanand Porika <Sabapatisourabnithyanand.Porika+ghub@blueyonder.com>
2026-03-01 17:00:12 +0530

    few code changes

--- src/main/java/com/example/ft/caloriestracking/controller/CalorieController.java
+++ src/main/java/com/example/ft/caloriestracking/controller/CalorieController.java
@@ -2,12 +2,14 @@ package com.example.ft.caloriestracking.controller;
 import com.example.ft.caloriestracking.dto.DayIntakeRequest;
 import com.example.ft.caloriestracking.dto.DayIntakeResponse;
 import com.example.ft.caloriestracking.service.CalorieCalculationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/calories")
 public class CalorieController {
 
@@ -20,5 +22,24 @@ public class CalorieController {
     @PostMapping("/calculate")
     public DayIntakeResponse calculate(@RequestBody DayIntakeRequest request) {
         return service.calculate(request);
+    }
+
+    @PostMapping("/totalCalories")
+    public ResponseEntity<Object> getTotalCalories(
+            @RequestBody DayIntakeRequest request) {
+
+        if (!StringUtils.hasText(request.getDate())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("date is mandatory");
+        }
+
+        if (request.getUserId() == null) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("userId is mandatory");
+        }
+
+        return ResponseEntity.ok(service.getTotalCalories(request));
     }
 }
